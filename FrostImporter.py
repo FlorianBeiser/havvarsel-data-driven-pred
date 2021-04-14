3b92a6ba34452002330a567989bc38e3d0cae611
Florian Beiser <florianb@r740-dsxm2t2-ag-gcompute.int.met.no>
2021-04-14 15:52:12 +0000

    Allowing to fetch multiple parameters

--- FrostImporter.py
+++ FrostImporter.py
@@ -12,7 +12,7 @@ Test frost.met.no (observations) - THIS TAKES A COUPLE OF MINUTES TO RUN:
 (other available params we have discussed to include: wind_speed and relative_humidity and cloud_area_fraction and sum(duration_of_sunshinePT1H) or mean(surface_downwelling_shortwave_flux_in_air PT1H) )
 
 Test for the construction of a data set:
-'python FrostImporter.py --fab all -id 1 -param air_temperature -S 2019-01-01T00:00 -E 2019-12-31T23:59'
+'python FrostImporter.py --fab all -id 1 -param air_temperature -n 10 -param "sum(duration_of_sunshine PT1H) -n 5 -S 2019-01-01T00:00 -E 2019-12-31T23:59'
 
 TODO:
  - Tune processing and storing of observational data sets (to suite whatever code that will use the data sets)
@@ -40,48 +40,35 @@ class FrostImporter:
         """
         # For command line calls the class reads the parameters from argsPars
         if start_time is None:
-            frost_api_base, station_id, param, start_time, end_time = self.__parse_args()
+            frost_api_base, station_id, params, ns, start_time, end_time = self.__parse_args()
 
             self.start_time = datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
             self.end_time = datetime.datetime.strptime(end_time, "%Y-%m-%dT%H:%M")
 
+
+            # FIXME: This feature (the selection of frost_api_base) may get obsolete 
             # keyword "all" triggers workflow to construct a csv file
-            # containing the water_temperature series of selected station of havvarsel frost
-            # and adds params time series from the 5 closest frost stations
+            # containing the water_temperature series of the selected station of havvarsel frost
+            # and adds params time series from the n closest frost stations 
             if frost_api_base == "all":
-                print("Starting the construction of an data set...")
-                # meta data and time series from havvarsel frost
-                print("The Havvarsel Frost observation site:")
-                location, data = self.havvarsel_frost(station_id)
-                # identifying closest station_id's on frost
-                print("The closest Frost stations:")
-                frost_station_ids = self.frost_location_ids(location, 10, param)
-                # Fetching data for those ids and add them to data
-                for i in range(len(frost_station_ids)):
-                    # NOTE: Some sites have interrupted timeseries. By default only the latest part is fetched...
-                    # TODO: Fetch data also for concatinated timeseries 
-                    #---
-                    # NOTE: Per call a maximum of 100.000 observations can be fetched at once
-                    # Some time series exceed this limit.
-                    # TODO: Fetch data year by year to stay within the limit 
-                    print("Fetching data for ", frost_station_ids[i])
-                    timeseries = self.frost(frost_station_ids[i],param)
-                    if timeseries is not None:
-                        print("Postprocessing the fetched data...")
-                        data = self.postprocess_frost(timeseries,frost_station_ids[i],param,data)
-                        print("Done. (Data is added to the data set)")
-                        data.to_csv("data_save_intermediate"+str(i)+".csv")
-                # save dataset
-                print("Dataset is constructed and will be saved now...")
-                data.to_csv("dataset_"+param+".csv")
-                print("Ready!")
+                # Check sanity of input
+                assert len(params) == len(ns), "Specified number of parameters does not match the radii"
+                # Construct dataset
+                self.constructDataset(station_id, params, ns)
 
             else:
-                # switch between Frost instances/servers
-                if "havvarsel" in frost_api_base:
-                    self.havvarsel_frost(station_id, param, frost_api_base, self.start_time, self.end_time)
-                else:
-                    self.frost(station_id, param, frost_api_base, self.start_time, self.end_time)
+                for ip in range(len(params)):
+                    param = params[ip]
+                    # switch between Frost instances/servers
+                    if "havvarsel" in frost_api_base:
+                        assert len(params)==1, "Havvarsel-Frost only allows the single parameter \"temperature\""
+                        _, data = self.havvarsel_frost(station_id, param, frost_api_base, self.start_time, self.end_time)
+                        data.to_csv("dataset.csv")
+                    else:
+                        data = self.frost(station_id, param, frost_api_base, self.start_time, self.end_time)
+                        if data is not None:
+                            data.to_csv("dataset_"+param+".csv")
+
         
         # Non-command line calls expect start and end_time to initialise a valid instance
         else:
@@ -157,7 +144,7 @@ class FrostImporter:
     def frost_location_ids(self, havvarsel_location, n, param=None, client_id='3cf0c17c-9209-4504-910c-176366ad78ba'):
         """Identifying the n closest station_ids in the Frost database around havvarsel_locations"""
 
-        # Fetching location data from frost
+        # Fetching source data from frost for the given param 
         url = "https://frost.met.no/sources/v0.jsonld"
         if param is not None:
             payload = {"validtime":str(self.start_time.date())+"/"+str(self.end_time.date()),
@@ -183,6 +170,8 @@ class FrostImporter:
                 df = df.append(row)
 
         df = df.reset_index()
+
+        # Fetching 
 
         # Building data frame with coordinates and distances with respect to havvarsel_location
         latlon_ref = (float(havvarsel_location["lat"][0]),float(havvarsel_location["lon"][0]))
@@ -254,10 +243,12 @@ class FrostImporter:
         # NOTE: The Frost data commonly holds observations for more times 
         # than the referenced Havvarsel Frost timeseries.
         # Extracting observations only for relevant times 
+        # TODO: match cloud_area_fraction timeseries by hand
         if "time" not in data.columns:
             data = data.reset_index()
         timeseries = timeseries.loc[timeseries['referenceTime'].isin(data["time"])]
         timeseries = timeseries.set_index("referenceTime")
+
 
         # Check if time series have same length
         if len(data)==len(timeseries):
@@ -283,6 +274,40 @@ class FrostImporter:
         return data
 
 
+    def constructDataset(self, station_id, params, ns):
+        """ construct a csv file containing the water_temperature series of the selected station of havvarsel frost
+        and adds params time series from the n closest frost stations
+        """
+        print("-------------------------------------------")
+        print("Starting the construction of an data set...")
+        # meta data and time series from havvarsel frost
+        print("The Havvarsel Frost observation site:")
+        location, data = self.havvarsel_frost(station_id)
+        for ip in range(len(params)):
+            param = params[ip]
+            n = int(ns[ip])
+            print("-------------------------------------------")
+            print("Parameter: ",param)
+            # identifying closest station_id's on frost
+            print("The closest ",n," Frost stations:")
+            frost_station_ids = self.frost_location_ids(location, n, param)
+            # Fetching data for those ids and add them to data
+            for i in range(len(frost_station_ids)):
+                # NOTE: Per call a maximum of 100.000 observations can be fetched at once
+                # Some time series exceed this limit.
+                # TODO: Fetch data year by year to stay within the limit 
+                print("Fetching data for ", frost_station_ids[i])
+                timeseries = self.frost(frost_station_ids[i],param)
+                if timeseries is not None:
+                    print("Postprocessing the fetched data...")
+                    data = self.postprocess_frost(timeseries,frost_station_ids[i],param,data)
+                    print("Done. (Data is added to the data set)")
+        # save dataset
+        print("Dataset is constructed and will be saved now...")
+        data.to_csv("dataset.csv")
+        print("Ready!")
+
+
 
     @staticmethod
     def __parse_args():
@@ -294,8 +319,11 @@ class FrostImporter:
             '-id', dest='station_id', required=True,
             help='fetch data for station with given id')
         parser.add_argument(
-            '-param', required=True,
+            '-param', required=True, action='append',
             help='fetch data for parameter')
+        parser.add_argument(
+            '-n', action='append',
+            help='number of closest frost stations to consider')
         parser.add_argument(
             '-S', '--start-time', required=True,
             help='start time in ISO format (YYYY-MM-DDTHH:MM) UTC')
@@ -303,7 +331,7 @@ class FrostImporter:
             '-E', '--end-time', required=True,
             help='end time in ISO format (YYYY-MM-DDTHH:MM) UTC')
         res = parser.parse_args(sys.argv[1:])
-        return res.frost_api_base, res.station_id, res.param, res.start_time, res.end_time
+        return res.frost_api_base, res.station_id, res.param, res.n, res.start_time, res.end_time
 
 if __name__ == "__main__":
 
