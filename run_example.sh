ead51d34ecd8a4da9a799edaddf30148f93cf74c
Florian Beiser <florianb@r740-dsxm2t2-ag-gcompute.int.met.no>
2021-04-18 10:23:13 +0000

    prelim dataset for MA8701 at NTNU

--- run_example.sh
+++ run_example.sh
@@ -9,18 +9,24 @@
 
 ## Execute example run
 param1="air_temperature"
-n1="10"
+n1=4
 
 param2="wind_speed"
-n2=10
+n2=3
 
 param3="cloud_area_fraction"
-n3=5
+n3=3
 
-param4="mean(surface_downwelling_shortwave_flux_in_air PT1H)"
-n4=3
+param4="mean(solar_irradiance PT1H)"
+n4=1
 
 param5="sum(duration_of_sunshine PT1H)"
-n5=3
+n5=2
 
-python FrostImporter.py --fab all -id 1 -param "$param1" -n $n1 -param "$param2" -n $n2 -param "$param3" -n $n3 -param "$param4" -n $n4 -param "$param5" -n $n5 -S 2016-01-01T00:00 -E 2020-12-31T23:59
+param6="mean(relative_humidity PT1H)"
+n6=2
+
+param7="mean(surface_downwelling_shortwave_flux_in_air PT1H)"
+n7=1
+
+python FrostImporter.py --fab all -id 1 -param "$param1" -n $n1 -param "$param2" -n $n2 -param "$param3" -n $n3 -param "$param4" -n $n4 -param "$param5" -n $n5 -param "$param6" -n $n6 -param "$param7" -n $n7 -S 2016-01-01T00:00 -E 2020-12-31T23:59
